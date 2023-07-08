71f3a3f999e5aec8c53946128797740c21cfdbdf
torresgarcia47 <138963966+torresgarcia47@users.noreply.github.com>
2023-07-08 00:31:17 -0600

    initial commit

---
+++ java/java.js
@@ -0,0 +1,8 @@
+function mostrarD1(){
+    document.getElementById('DetalleP1').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD1(){
+    document.getElementById('DetalleP1').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
