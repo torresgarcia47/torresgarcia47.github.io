f03166bb5f149fddec4c7c3303fe399969037ef6
torresgarcia47 <138963966+torresgarcia47@users.noreply.github.com>
2023-07-19 20:35:39 -0600

    Add files via upload

---
+++ java/java.js
@@ -0,0 +1,231 @@
+function mostrarD1(){
+    document.getElementById('DetalleP1').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD1(){
+    document.getElementById('DetalleP1').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD2(){
+    document.getElementById('DetalleP2').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD2(){
+    document.getElementById('DetalleP2').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD3(){
+    document.getElementById('DetalleP3').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD3(){
+    document.getElementById('DetalleP3').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD4(){
+    document.getElementById('DetalleP4').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD4(){
+    document.getElementById('DetalleP4').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD5(){
+    document.getElementById('DetalleP5').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD5(){
+    document.getElementById('DetalleP5').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD6(){
+    document.getElementById('DetalleP6').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD6(){
+    document.getElementById('DetalleP6').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD7(){
+    document.getElementById('DetalleP7').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD7(){
+    document.getElementById('DetalleP7').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD8(){
+    document.getElementById('DetalleP8').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD8(){
+    document.getElementById('DetalleP8').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+function mostrarD9(){
+    document.getElementById('DetalleP9').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD9(){
+    document.getElementById('DetalleP9').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD10(){
+    document.getElementById('DetalleP10').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD10(){
+    document.getElementById('DetalleP10').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD11(){
+    document.getElementById('DetalleP11').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD11(){
+    document.getElementById('DetalleP11').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD12(){
+    document.getElementById('DetalleP12').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD12(){
+    document.getElementById('DetalleP12').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD13(){
+    document.getElementById('DetalleP13').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD13(){
+    document.getElementById('DetalleP13').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostroD14(){
+    document.getElementById('DetalleP14').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD14(){
+    document.getElementById('DetalleP14').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+function mostrarD15(){
+    document.getElementById('DetalleP15').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD15(){
+    document.getElementById('DetalleP15').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD16(){
+    document.getElementById('DetalleP16').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD16(){
+    document.getElementById('DetalleP16').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD17(){
+    document.getElementById('DetalleP17').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD17(){
+    document.getElementById('DetalleP17').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD18(){
+    document.getElementById('DetalleP18').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD18(){
+    document.getElementById('DetalleP18').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD19(){
+    document.getElementById('DetalleP19').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD19(){
+    document.getElementById('DetalleP19').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD20(){
+    document.getElementById('DetalleP20').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD20(){
+    document.getElementById('DetalleP20').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD21(){
+    document.getElementById('DetalleP21').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD21(){
+    document.getElementById('DetalleP21').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD22(){
+    document.getElementById('DetalleP22').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD22(){
+    document.getElementById('DetalleP22').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD23(){
+    document.getElementById('DetalleP23').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD23(){
+    document.getElementById('DetalleP23').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD24(){
+    document.getElementById('DetalleP24').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD24(){
+    document.getElementById('DetalleP24').style.display='none';
+    document.getElementById('Universalgris24').style.display='none';
+}
+
+function mostrarD25(){
+    document.getElementById('DetalleP25').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD25(){
+    document.getElementById('DetalleP25').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
+
+function mostrarD26(){
+    document.getElementById('DetalleP26').style.display='block';
+    document.getElementById('Universalgris').style.display='block';
+}
+function cerrarD26(){
+    document.getElementById('DetalleP26').style.display='none';
+    document.getElementById('Universalgris').style.display='none';
+}
