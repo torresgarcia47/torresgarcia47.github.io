be4c4aa45ab7ce6d64a69e5412433110b58e4da4
torresgarcia47 <138963966+torresgarcia47@users.noreply.github.com>
2023-07-19 20:32:57 -0600

    Delete java directory

--- java/java.js
+++
@@ -1,8 +0,0 @@
-function mostrarD1(){
-    document.getElementById('DetalleP1').style.display='block';
-    document.getElementById('Universalgris').style.display='block';
-}
-function cerrarD1(){
-    document.getElementById('DetalleP1').style.display='none';
-    document.getElementById('Universalgris').style.display='none';
-}
